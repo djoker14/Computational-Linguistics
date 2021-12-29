e462b9768e5df5232b7f9310147a1b42087321f6
djoker14 <66789140+djoker14@users.noreply.github.com>
2021-12-29 17:53:35 +0100

    Update type-token-ratio.py

--- my-projects/code/type-token-ratio.py
+++ my-projects/code/type-token-ratio.py
@@ -1,1 +1,21 @@
+import collections
+from collections import Counter
 
+discorsi = open("speeches.txt", mode = "r", encoding = "utf-8")
+lettura = discorsi.read().lower()
+parole = lettura.split()
+parole.sort()
+
+
+def frequenza_parole(parole):
+    counts = Counter(parole)
+    return counts
+
+def frequenza_ordinata(lista):
+    ordine = collections.Counter(lista).most_common()
+    return ordine[:50]
+
+tokens = len(parole)
+type_words = len(set(parole))
+type_token_ratio = type_words/tokens
+print(type_token_ratio)
